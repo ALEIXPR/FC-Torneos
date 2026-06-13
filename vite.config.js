9f904cae546392be086b222f6f1a3709815b99d9
AleixPR <aleixpr10@gmail.com>
2026-06-13 22:11:03 +0200

    Fix local vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/FC-Torneos/',
+  base: process.env.NODE_ENV === 'production' ? '/FC-Torneos/' : '/',
   plugins: [
     react(),
     VitePWA({ 
